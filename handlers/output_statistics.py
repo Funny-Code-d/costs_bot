d269f996adbc971a0a928ed274ae8e583fadcdf4
Funny-Code-d <sosnin_dienis@mail.ru>
2021-07-23 22:42:44 +0700

    Временное решение слишком длинных сообщений

--- handlers/output_statistics.py
+++ handlers/output_statistics.py
@@ -10,7 +10,14 @@ from moduls import expences
 @dp.message_handler(lambda message: message.text.startswith('Вывести покупки за сегодня'))
 async def get_statistics_day(message: Message):
 	answer = expences.output_today_buy(message.from_user.id)
-	await message.answer(answer, parse_mode=ParseMode.MARKDOWN)
+	if len(answer) > 4096:
+    	for x in range(0, len(answer), 4096):
+        	#bot.send_message(message.chat.id, info[x:x+4096])
+        	await message.answer(answer[x:x+4096], parse_mode=ParseMode.MARKDOWN)
+	else:
+    	#bot.send_message(message.chat.id, info)
+    	await message.answer(answer, parse_mode=ParseMode.MARKDOWN)
+	#await message.answer(answer, parse_mode=ParseMode.MARKDOWN)
 
 #--------------------------------------------------------------------------------------------
 
